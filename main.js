8c3e1f9e634f3bd9726a52b27572c3474ca5032b
E. M. Greeff <893946+modster@users.noreply.github.com>
2022-03-26 07:32:38 -0600

    added vuehub files

--- main.js
+++ main.js
@@ -1,6 +1,19 @@
-import './style.css'
+import { select } from 'd3';
 
-document.querySelector('#app').innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`
+const svg = select('svg');
+
+const width = document.body.clientWidth;
+const height = document.body.clientHeight;
+
+svg
+    .attr('width', width)
+    .attr('height', height)
+  .append('rect')
+    .attr('width', width)
+    .attr('height', height)
+    .attr('rx', 40)
+
+// document.querySelector('#app').innerHTML = `
+//   <h1>Hello Vite!</h1>
+//   <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
+// `
