2c8e98f63081f311770567c42afb69eaa5725128
E. M. Greeff <893946+modster@users.noreply.github.com>
2022-03-26 18:15:06 -0600

    fragment shader

--- main.js
+++ main.js
@@ -1,19 +1,14 @@
-import { select } from 'd3';
+import * as THREE from 'three';
+// import { select } from 'd3';
 
-const svg = select('svg');
+// const svg = select('svg');
+// const width = document.body.clientWidth;
+// const height = document.body.clientHeight;
 
-const width = document.body.clientWidth;
-const height = document.body.clientHeight;
-
-svg
-    .attr('width', width)
-    .attr('height', height)
-  .append('rect')
-    .attr('width', width)
-    .attr('height', height)
-    .attr('rx', 40)
-
-// document.querySelector('#app').innerHTML = `
-//   <h1>Hello Vite!</h1>
-//   <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-// `
+// svg
+//     .attr('width', width)
+//     .attr('height', height)
+//   .append('rect')
+//     .attr('width', width)
+//     .attr('height', height)
+//     .attr('rx', 40)
